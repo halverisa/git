d89e327eea864ef49af20d754539db8dc5213d22
halvert medina <halverisa@gmail.com>
2018-06-05 14:55:15 -0500

    animacion_titulo
    animacion del titulo cambiando de color

--- evalfinalframeworkjs/js/app.js
+++ evalfinalframeworkjs/js/app.js
@@ -1,3 +1,16 @@
+//funcion para animar texto
+var vueltas = function(){
+  $(".main-titulo").animate({
+   color: "#FFFFFF"
+}, 2500,function(){
+      $(this).animate({
+        color:"#FFE900"},2500)
+      });
+}
+var bucle = function(){
+  vueltas();
+  setTimeout("bucle();",7000)
+}
 $(document).ready(function(){
-  alert("hola esta funzu");
+  bucle();
 });

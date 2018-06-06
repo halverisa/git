1a80753b04b56f5751da2d2072effd59543cd00c
halvert medina <halverisa@gmail.com>
2018-06-06 16:54:18 -0500

    funcion_draggable

--- evalfinalframeworkjs/js/app.js
+++ evalfinalframeworkjs/js/app.js
@@ -13,28 +13,30 @@ var bucle = function(){
 }
 $(document).ready(function(){
   bucle();
+
 });
 
 //funcion para cambiar texto boton iniciar
 $(function(){
   $(".btn-reinicio").click(function(){
     $(".btn-reinicio").text("Reiniciar");
+    $(".btn-reinicio").click(function(){
+      location.reload();
+    });
     //generar numero aleatorio
-    var a=1
-    var b=4
-    var aleatorio = Math.round(Math.random()*(b-a)+parseInt(a));
-    var aleatorio1 = Math.round(Math.random()*(b-a)+parseInt(a));
-    var aleatorio2 = Math.round(Math.random()*(b-a)+parseInt(a));
-    var aleatorio3 = Math.round(Math.random()*(b-a)+parseInt(a));
-    var aleatorio4 = Math.round(Math.random()*(b-a)+parseInt(a));
+    var aleatorio = Math.floor(Math.random()*4)+1;
+    var aleatorio1 = Math.floor(Math.random()*4)+1;
+    var aleatorio2 = Math.floor(Math.random()*4)+1;
+    var aleatorio3 = Math.floor(Math.random()*4)+1;
+    var aleatorio4 = Math.floor(Math.random()*4)+1;
+    $(".cuadro, .elemento, .panel-tablero").draggable({
+      grid: [120,90],
+      revert: "valid"
+    });
     // cargar div con imagenes
-     //$("[class^='col-']").html("<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+
-     $(".col-1, .col-4, .col-7").html("<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+
-       "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3+".png id=dulce></img></div>");
-       $(".col-2, .col-5").html("<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+
-         "<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio1+".png id=dulce></img></div>");
-         $(".col-3, .col-6").html("<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+
-           "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>");
+     $(".col-1, .col-4, .col-7").html("<div class= 'cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>"+"<div class= 'cuadro'><img src=image/"+aleatorio1+".png class='elemento' class=dulce></img></div>"+ "<div class='cuadro'><img src=image/"+aleatorio2+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio4+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio1+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio4+".png class='elemento'></img></div>");
+    //$(".col-2, .col-5").html("<div class='cuadro'><img src=image/"+aleatorio4+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio3+".png class='elemento'></img></div>"+ "<div class='cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio2+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio1+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio4+".png class='elemento'></img></div>");
+    // $(".col-3, .col-6").html("<div><img src=image/"+aleatorio2+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>"+ "<div class='cuadro'><img src=image/"+aleatorio1+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio3+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio4+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio2+".png class='elemento'></img></div>"+"<div class='cuadro'><img src=image/"+aleatorio+".png class='elemento'></img></div>");
 
   })
 })

8c94780a351120679ba4a0ffd2b8cd06750b860e
halvert medina <halverisa@gmail.com>
2018-06-05 20:30:42 -0500

    llenado_tablero_aleatorio

--- evalfinalframeworkjs/js/app.js
+++ evalfinalframeworkjs/js/app.js
@@ -14,4 +14,34 @@ var bucle = function(){
 $(document).ready(function(){
   bucle();
 });
-//llenar tablero con dulce aleatoriamente
+
+//funcion para cambiar texto boton iniciar
+$(function(){
+  $(".btn-reinicio").click(function(){
+    $(".btn-reinicio").text("Reiniciar");
+    //generar numero aleatorio
+    var a=1
+    var b=4
+    var aleatorio = Math.round(Math.random()*(b-a)+parseInt(a));
+    var aleatorio1 = Math.round(Math.random()*(b-a)+parseInt(a));
+    var aleatorio2 = Math.round(Math.random()*(b-a)+parseInt(a));
+    var aleatorio3 = Math.round(Math.random()*(b-a)+parseInt(a));
+    var aleatorio4 = Math.round(Math.random()*(b-a)+parseInt(a));
+    // cargar div con imagenes
+     //$("[class^='col-']").html("<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+
+     $(".col-1").html("<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+
+       "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3+".png id=dulce></img></div>");
+       $(".col-2").html("<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+
+         "<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio1+".png id=dulce></img></div>");
+         $(".col-3").html("<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+
+           "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>");
+           $(".col-4").html("<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+
+             "<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3 +".png id=dulce></img></div>");
+             $(".col-5").html("<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+
+               "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio2 +".png id=dulce></img></div>");
+               $(".col-6").html("<div><img src=image/"+aleatorio4+".png id=dulce></img></div>"+
+                 "<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio1+".png id=dulce></img></div>");
+                 $(".col-7").html("<div><img src=image/"+aleatorio2+".png id=dulce></img></div>"+
+                   "<div><img src=image/"+aleatorio+".png id=dulce></img></div>"+ "<div><img src=image/"+aleatorio1+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio3+".png id=dulce></img></div>"+"<div><img src=image/"+aleatorio4+".png id=dulce></img></div>");
+  })
+})

0e67b027de951ebb8db323ad7ff0fb675a4c0fe6
halvert medina <halverisa@gmail.com>
2018-06-07 09:14:53 -0500

    llenado_tablero
    cargando imagenes al tablero de manera aleatoria

--- evalfinalframeworkjs/js/app.js
+++ evalfinalframeworkjs/js/app.js
@@ -43,27 +43,48 @@ $(document).ready(function(){
 var imagenes = ["image/1.png","image/2.png", "image/3.png", "image/4.png"];
 var numimagenes = imagenes.length;
 var imgpuesta=0;
+var dimension=7;
+var matriz =[];
 
 $(function(){
 
 //imagenes aleatorias
   function imagenAleatoria() {
       imgpuesta = Math.floor((Math.random() * numimagenes));
-      alert(imgpuesta);
       return imagenes[imgpuesta];
 
   }
 // funcion crear tablero de imagenes
- function creardivs() {
-   imagenAleatoria();
-   $(".col-1, .col-4, .col-7").html("<div class= 'cuadro'><img src=image/"+imgpuesta+".png class='elemento'></img></div>");
+function juego(f, c, obj, src)
+{
+    return {
+        f: f,
+        c: c,
+        fuente: src,
+        enCombo: false,
+        o: obj
+    };
+}
+
+ function creartablero() {
+     for (var f = 0; f < dimension; f++) {
+         matriz[f] = [];
+         for (var c = 0; c < dimension; c++) {
+
+             matriz[f][c] = new juego(f, c, null, imagenAleatoria());
+
+             var celda = $('#img_' + f + '_' + c).html("<img class='elemento' src='" + matriz[f][c].fuente + "' alt='" + f + "," + c + "'/>");
+             matriz[f][c].o = celda;
+         }
+     }
  }
 
  // funcion para boton iniciar
  $(".btn-reinicio").click(btniniciar);
  function btniniciar(){
      $('.btn-reinicio').text('Reiniciar');
-     creardivs();
+     //creardivs();
+     creartablero();
      $('.btn-reinicio').click(function(){
        location.reload();
      })
